6326868ccf1307633cac894909e4af84c002f60e
James Call <jacall2012@byui.edu>
2024-01-12 22:50:59 -0700

    changed html file location

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./**/*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
